bba8fc41bc7ba264e97145de657d9a607b5c8824
Tyler Bifolchi <TJBifolchi@outlook.com>
2025-03-05 11:50:19 -0500

    Update taskmanager.h

--- src/taskmanager.h
+++ src/taskmanager.h
@@ -3,13 +3,10 @@
 
 #include <windows.h>
 
-// Function to create the task manager window and list running processes
 void CreateTaskManagerWindow(HWND hwnd);
 
-// Function to terminate a process by its PID
 void TerminateProcessByPid(HWND hwnd, DWORD processId);
 
-// Function to refresh the process list
 void RefreshProcessList(HWND hwndList);
 
 #endif // TASK_MANAGER_H
